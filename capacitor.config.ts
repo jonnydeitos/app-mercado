a6df6f73f5635c3af54e94a1532cf9574677a243
jonnydeitos <jonnydeitos@gmail.com>
2025-03-28 11:59:19 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mercado',
+  webDir: 'www'
+};
+
+export default config;
